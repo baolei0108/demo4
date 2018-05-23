be67439e5a566cef98770f418c2fe58c441b9afd
baolei <1141523588@qq.com>
2018-05-23 13:34:14 +0800

    demo3

---
+++ demo3/app.vue
@@ -0,0 +1,47 @@
+<template>
+<div class="daily">
+
+    <my-component></my-component>
+    <my-component></my-component>
+    
+    <input type="text" v-model="parentMessage"></input>
+    <component is="first-component" :waring-text="parentMessage"></component>
+
+    <table>
+       <tbody is="first-component" waring-text="提示信息"></tbody>
+    </table>
+
+    
+</div>
+
+
+</template>
+
+
+
+
+<script>
+   import $ from './libs/util';
+   import myComponent from './components/my-component.vue';
+   import firstComponent from './components/first-component.vue';
+
+   export default{
+       components:{myComponent,firstComponent},
+       data(){
+           return {
+               parentMessage: '',
+           }
+
+       },
+
+       methods: {
+           
+        
+
+
+       },
+
+       
+}
+
+</script>
